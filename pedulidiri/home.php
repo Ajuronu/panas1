cb6bdba220ff681da2865cbb62a645757067cf75
Ajuronu <106498051+Ajuronu@users.noreply.github.com>
2022-11-22 14:03:34 +0700

    Update home.php

--- pedulidiri/home.php
+++ pedulidiri/home.php
@@ -4,8 +4,15 @@ session_start();
 ?>
 
 <html>
+    <link rel="stylesheet" href="style.css">
     <body>
-        <table align="center" border="1" height="30%" width="50%">
-            <td align="center" valign="top">- Selamat datang <?php echo $_SESSION['username']?> di aplikasi Peduli Diri -</td>
+        <table align="center" border="1" height="30%" width="45%">
+            <td align="center" valign="top">- Selamat datang <?php echo $_SESSION['username']?> di Aplikasi Peduli Diri -</td>
+        </table>
+        <table width="982px">
+            <tr>
+                <td align="right"><a href="isidata.php"><input type="submit" name="isi" value="Isi Catatan Perjalanan"></td>
+            </tr>
         </table>
     </body>
+</html>
