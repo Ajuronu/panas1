9ffa914631e234652a4af7a05597980ebd001e83
Ajuronu <106498051+Ajuronu@users.noreply.github.com>
2022-11-22 19:01:58 +0700

    Update header.php

--- pedulidiri/header.php
+++ pedulidiri/header.php
@@ -1,11 +1,12 @@
 <h4 align="right"><a href="logout.php">Logout</a></h4>
 <table align="center" width="70%">
     <tr>
-        <td width="175px"><img src="icon/map.png" alt="" width="144px"></td>
+        <td width="175px"><img src="icon/img.png" alt="" width="144px">
+        </td>
         <td>
             <h2>PEDULI DIRI</h2>
             <p>Catatan Perjalanan</p>
-            <p><a href="home.php">Home</a>  |   <a href="catatan.php">Catatan Perjalanan</a>    |   <a href="isidata.php">Isi Data</a></p>
+            <p><a href="home.php">Home</a> | <a href="catatan.php">Catatan Perjalanan</a> | <a href="isidata.php">Isi Data</a></p>
         </td>
     </tr>
 </table>
